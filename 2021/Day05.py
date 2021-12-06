bea9b560aa9ff7cd06062a0389502fb234e5d583
mingweiniu <mingweiniu@gmail.com>
2021-12-06 14:59:47 +0800

    Update Day05.py

--- 2021/Day05.py
+++ 2021/Day05.py
@@ -7,19 +7,18 @@ with open('input.txt', 'r') as input_file:
     pass
 
 days = 256
+# wait one more day to create new fish
 reset_timer = 6 + 1
 new_timer = 8 + 1
 
-
+# initialize tables
 fabonacci = [1]
 fabonacci += [2] * reset_timer
 fabonacci += [3] * (new_timer - reset_timer)
 fabonacci += list(range(days))
 
 for d in range(new_timer + 1, len(fabonacci)):
-    fab = fabonacci
-    fab[d] = fab[d - new_timer] + fab[d - reset_timer]
+    f = fabonacci
+    f[d] = f[d - new_timer] + f[d - reset_timer]
 
-res_each_item = [fabonacci[days - i] for i in inputs]
-
-print(sum(res_each_item))
+print(sum([fabonacci[days - i] for i in inputs]))
