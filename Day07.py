20ea9abab62e43b50d8f040491ee49d7b43ba281
mingweiniu <mingweiniu@gmail.com>
2021-12-07 19:27:18 +0800

    Update Day07.py

--- Day07.py
+++ Day07.py
@@ -6,12 +6,11 @@ with open('input.txt', 'r') as input_file:
         inputs = np.array([int(i) for i in line.split(',')])
     pass
 
-inputs -= min(inputs)
-
 # part 1
 print(min([sum(abs(inputs - i)) for i in inputs]))
 
 # part 2
+inputs -= min(inputs)
 max_i = max(inputs)
 n = np.array(range(max_i + 1))
 table = (n + 1) * n // 2
