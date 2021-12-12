24b659d2c5336aad8577b48137972fc32f68b4bd
mingweiniu <mingweiniu@gmail.com>
2021-12-12 22:00:23 +0800

    Update Day12Failing.py

--- Day12Failing.py
+++ Day12Failing.py
@@ -4,6 +4,7 @@ with open('input.txt', 'r') as input_file:
         input_pairs.append(set([i for i in line[:-1].split('-')]))
     pass
 
+
 # create graph
 neighbors = {}
 calculate_table = {}
@@ -20,32 +21,33 @@ for i in set.union(*input_pairs):
 
 
 def FindPath(path, calculate_table, result_paths):
-    calculate_table[path[-1]] +=1
+    calculate_table = calculate_table.copy()   
 
-    if path[-1] == 'end':
+    current_node = path[-1]
+    calculate_table[current_node] +=1
+
+    if current_node == 'end':
         result_paths.append(path)
-        print(calculate_table)
-        print(path)
         return True
+    
+    if sum(calculate_table.values()) > len(calculate_table) + 1:
+        return False
+    
 
-    else:
-        if sum(calculate_table.values()) > len(calculate_table) + 1:
-            return False
-
-        print('node ' + str(path[-1]) + ' neighbors are ' + str(neighbors[path[-1]]))
-
-        for next_node in neighbors[path[-1]]:
-            if next_node.islower() and calculate_table[next_node]:
-                pass
-            else:
-                calculate_table = calculate_table.copy()
-                print('path ' + str(path) + ' append ' + str(next_node))
-                path = path.copy()
-                path.append(next_node)
-                FindPath(path, calculate_table, result_paths)
-    pass
+    for next_node in neighbors[current_node]:
+        if next_node.islower() and calculate_table[next_node]:
+            pass
+        else:
+            path_local = path.copy()
+            path_local.append(next_node)
+            FindPath(path_local, calculate_table, result_paths)
+    return False
 
 result_paths = []
+
 FindPath(['start'], calculate_table, result_paths)
 
-test = 'test'
+for path in result_paths:
+    print(path)
+
+print(len(result_paths))
