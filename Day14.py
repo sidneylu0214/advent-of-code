ffa99e4e079eee5e408ab1da3171f298491bc43f
mingweiniu <mingweiniu@gmail.com>
2021-12-14 16:26:48 +0800

    Update and rename Day14debugging.py to Day14.py

--- Day14.py
+++ Day14.py
@@ -31,7 +31,6 @@ def Apply(table, input_string):
 output = input_pairs[0]
 for i in range(10):
     output = Apply(transition, output)
-    print(len(output))
     pass
 
 cnt = [output.count(a) for a in string.ascii_uppercase if output.count(a) > 0]
@@ -52,11 +51,11 @@ matrix_shape = [len(transition), len(transition)]
 base_martix = np.zeros(matrix_shape, dtype=np.ulonglong)
 
 for key in transition.keys():
-    x_part = convert_num[key]
-    y_parts = [convert_num[k] for k in transition[key]]
+    y_part = convert_num[key]
+    x_parts = [convert_num[k] for k in transition[key]]
 
     test = 'test'
-    for y_part in y_parts:
+    for x_part in x_parts:
         base_martix[x_part][y_part] += 1
     pass
 
@@ -66,13 +65,29 @@ for i in range(len(input_pairs[0]) - 1):
     pair = (input_pairs[0])[i: i + 2]
     init_state[convert_num[pair]][0] += 1
 
-
-for i in range(10):
+for i in range(40):
     init_state = np.matmul(base_martix, init_state)
-    print(sum(init_state))
 
 output = list(itertools.chain(*init_state.tolist()))
 
-cnt = [o for o in output if o > 0]
 
-print('part 2 is' + str(max(cnt) - min(cnt)))
+cnt_tables = {}
+for key in transition.keys():
+    a, b = key
+    cnt_tables[a] = 0
+    cnt_tables[b] = 0
+
+for key in transition.keys():
+    a, b = key
+    cnt = output[convert_num[key]]
+    cnt_tables[a] += cnt
+    cnt_tables[b] += cnt
+
+head = (input_pairs[0])[0]
+tail = (input_pairs[0])[-1]
+cnt_tables[head] += 1
+cnt_tables[tail] += 1
+
+diff = max(cnt_tables.values()) - min(cnt_tables.values())
+
+print('part 2 is' + str(diff // 2))
