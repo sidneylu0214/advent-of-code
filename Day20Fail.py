477fd4ea2393c5c8950d8c39f7643678bbc798f9
mingweiniu <mingweiniu@gmail.com>
2021-12-21 11:43:09 +0800

    Update Day20Fail.py

--- Day20Fail.py
+++ Day20Fail.py
@@ -1,25 +1,22 @@
 import numpy as np
 
-input_lines = []
-with open('input.txt', 'r') as input_file:
-    for line in input_file:
+intput_txt = []
+with open('input.txt', 'r') as intput_txt:
+    for line in intput_txt:
         input_lines.append([1 if i == '#' else 0 for i in line[:-1]])
     pass
 
 enhancement = input_lines[0]
 input_image = np.array(input_lines[2:], dtype = int)
 
-height, width = input_image.shape
 expand = 3
-c_expand = [np.zeros(height)] * expand
-r_expand = [[np.zeros(width + 2 * expand)]] * expand
-
+c_expand = [np.zeros(input_image.shape[0])] * expand
 input_image = np.c_[tuple(c_expand + [input_image]  + c_expand)]
+r_expand = [[np.zeros(input_image.shape[1])]] * expand
 input_image = np.r_[tuple(r_expand + [input_image] + r_expand)]
 input_image = np.array(input_image, dtype = int)
+
 height, width = input_image.shape
-
-
 def Enhance(input_image):
     result_image = np.zeros(input_image.shape, dtype = int)
     for y in range(1 , height - 1):
@@ -31,19 +28,9 @@ def Enhance(input_image):
 
             block = input_image[r_s:r_e + 1, c_s:c_e + 1]
             bin_string = ''.join(str(e) for e in block.flatten().tolist())
-            query_table = int(bin_string, 2)
-
-            result_image[y][x] = enhancement[query_table]
+            result_image[y][x] = enhancement[int(bin_string, 2)]
 
     return result_image
 
-
 res_image = Enhance(Enhance(input_image))
 print(np.where(res_image == 1)[0].size)
-
-print('0')
-print(input_image)
-print('1')
-print(Enhance(input_image))
-print('2')
-print(Enhance(Enhance(input_image)))
