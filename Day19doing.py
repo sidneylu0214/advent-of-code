595b55b95e73f8ffab4e3280dcebb3f43359ca76
mingweiniu <mingweiniu@gmail.com>
2021-12-22 11:39:21 +0800

    Update Day19doing.py

--- Day19doing.py
+++ Day19doing.py
@@ -1,38 +1,51 @@
 import numpy as np
-import re
+import itertools
 
-intput_txt = []
-with open('input.txt', 'r') as lines:
+intputs = []
+with open('input.txt', 'r') as intput_txt:
     in_views = []
-
-    for line in lines:
+    for line in intput_txt:
         if 'scanner' in line:
-            intput_txt.append(in_views.copy())
+            intputs.append(in_views.copy())
             in_views = []
         else:
             try:
-                in_views.append(np.array([line[:-1].split(',')], dtype = int))
+                in_views.append(np.array([int(i) for i in line[:-1].split(',')], dtype = np.longlong))
             except ValueError:
                 pass
-    intput_txt.append(in_views)
+    intputs.append(in_views)
+    intputs.pop(0)
     pass
 
-intput_txt = intput_txt[1:]
-
-def Dist(A):
-    A = A[0]
-    return np.dot(A, A)
-
-def GetDistanceSet(input):
-    distances = []
+def GetDistanceTable(input):
+    shape = [len(input), len(input)]
+    distances = np.zeros(shape, dtype = input[0].dtype)
 
     for i in range(len(input)):
         for j in range(len(input)):
-            distances.append(Dist(input[i] - input[j]))
-    
-    distances.sort()
-    return distances[::2]
+            x = input[i] - input[j]
+            distances[i][j] = (np.dot(x, x))
 
-dis = GetDistanceSet(intput_txt[0])
+    return distances
+
+tables = [GetDistanceTable(i) for i in intputs]
+
+def DistSet(table):
+    return set(list(itertools.chain(*table)))
+
+dist_sets = [DistSet(t) for t in tables]
+
+
+def TryMapping(dist_set, dist_table1, dist_table2):
+
+    pass
+
+for i in range(1, len(dist_sets)):
+    for j in range(len(dist_sets)):
+        intersect_set = sorted_dist[i].intersection(dist_sets[j])
+        if len(intersect_set) > 3:
+            TryMapping(intersect_set, )
+            pass
+
 
 test = 'test'
